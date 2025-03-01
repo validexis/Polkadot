4daed95571ce93c162091ae7d6f6ba9b4c28608e
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-03-01 17:35:30 +0200

    Update upgrademain.sh

--- upgrademain.sh
+++ upgrademain.sh
@@ -2,7 +2,7 @@
 sudo systemctl stop polkadot.service
 cd polkadot-sdk
 sudo git fetch
-git checkout polkadot-v1.17.1
+git checkout polkadot-v1.17.2
 cargo build --release
 sudo systemctl restart polkadot.service
 sudo journalctl -u polkadot.service -f
